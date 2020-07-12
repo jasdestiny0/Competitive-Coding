0231c9e5403539074d974d3ccc41343e32328c46
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-07-12 23:48:32 +0530

    Update solution.py

--- Leet code/Kadane's Algorithm/solution.py
+++ Leet code/Kadane's Algorithm/solution.py
@@ -1,15 +1,8 @@
-def hasSingleCycle(array):
-    length=len(array)
-	count=0
-	index=0
-	while count<length:
-		if index==0 and count!=0:
-			return False
-		count+=1
-		index+=array[index]
-		index%=length
-	return index==0
-			
-		
-		
-	
+def kadanesAlgorithm(Array):
+	MaxHere=Array[0]
+	MaxSoFar=Array[0]
+	for i in range(1,len(Array),1):
+		Number=Array[i]
+		MaxHere=max(Number,Number+MaxHere)
+		MaxSoFar=max(MaxSoFar,MaxHere)
+	return MaxSoFar
