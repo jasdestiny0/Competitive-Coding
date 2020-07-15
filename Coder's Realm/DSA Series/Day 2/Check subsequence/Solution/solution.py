a6a3d041de0f2637805573fea6c372d50b596ea9
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-07-15 20:57:43 +0530

    Create solution.py

---
+++ Coder's Realm/DSA Series/Day 2/Check subsequence/Solution/solution.py
@@ -0,0 +1,11 @@
+
+class Solution(object):
+    def isSubsequence(self, s, t):
+        indexS=0
+        indexT=0
+        while indexS<len(s) and indexT<len(t):
+            if s[indexS]==t[indexT]:
+                indexS+=1
+            indexT+=1
+    
+        return len(s)==indexS
