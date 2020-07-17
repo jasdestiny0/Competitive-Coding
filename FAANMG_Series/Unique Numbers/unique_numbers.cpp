c06370ae705116bc0cf8f2830e4bf358141db938
vishal-rfx <39836105+vishal-rfx@users.noreply.github.com>
2020-07-17 20:24:14 +0530

    Update unique_numbers.cpp

--- FAANMG_Series/Unique Numbers/unique_numbers.cpp
+++ FAANMG_Series/Unique Numbers/unique_numbers.cpp
@@ -1,3 +1,24 @@
+/*
+We are given an array containg n numbers. All the numbers are present twice except for two numbers which are present only once. Find the unique numbers in linear time without using any extra space. ( Hint - Use Bitwise )
+
+Input Format
+First line contains the number n. Second line contains n space separated number.
+
+Constraints
+n < 10^5
+
+Output Format
+Output a single line containing the unique numbers separated by space
+
+Sample Input
+4
+3 1 2 1
+Sample Output
+2 3
+Explanation
+Smaller number comes before larger number
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
