94a2b5b4e096bc5c6d96484f67c0621500eed4a7
vishal-rfx <39836105+vishal-rfx@users.noreply.github.com>
2020-07-21 21:33:42 +0530

    Update unique_characters.cpp

--- FAANMG_Series/Unique character String/unique_characters.cpp
+++ FAANMG_Series/Unique character String/unique_characters.cpp
@@ -1,11 +1,31 @@
-#include<iostream>
-#include<cstring>
-#include<vector>
-#include<algorithm>
+/*
+Ramu has an array of strings. He want to find a string s such that it is a concatenation of sub-sequence of given array and have unique characters.Since,he has just started coding so he needs your help.
+A sub-sequence of an array is a set of elements that can be obtained by deleting some elements(posssibly none) from the array and keeping the order same.
+Print the maximum possible length of s.
+
+Input Format
+The first line of input contains an interger n -the length of array.Next n lines contains the element(strings) of the array
+
+Constraints
+1 <= n <= 18, 1 <= arr[i].length<= 26
+
+Output Format
+Print one integer - the maximum length of s.
+
+Sample Input
+3
+ab
+cd
+ab
+Sample Output
+4
+*/
+#include<bits/stdc++.h>
 using namespace std;
 
 
 vector<string> v;
+vector<int> ans;
 
 int findMaxLengthSubsequence(int i,int n,string s){
     if(i==n){
@@ -25,16 +45,13 @@ int findMaxLengthSubsequence(int i,int n,string s){
         }
         return s.length();
     }
-    if((s.length()+v[i].length()<=26){
-        s1 = strcat(s,v[i]);
-        int h = findMaxLengthSubsequence(i+1,n,s1);
-        int v = findMaxLengthSubsequence(i+1,n,s);
-        return max(h,v);
+    int h=-1,k=-1;
+    if((s.length()+v[i].length()<=26)){
+        h = findMaxLengthSubsequence(i+1,n,s+v[i]);
 
     }
-    else{
-        return findMaxLengthSubsequence(i+1,n,s)
-    }
+    k = findMaxLengthSubsequence(i+1,n,s);
+    return max(h,k);
 
 }
 
@@ -50,7 +67,6 @@ int main(){
     }
 
     int ans = findMaxLengthSubsequence(0,n,"");
-    cout<<ans<<endl;
-    
+    cout<<ans<<endl;    
     return 0;
 }
