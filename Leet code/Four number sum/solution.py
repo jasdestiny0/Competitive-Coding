baa83ad2acd45cb5f6ecc9e8f3720913c2aded4b
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-07-23 23:33:03 +0530

    Create solution.py

---
+++ Leet code/Four number sum/solution.py
@@ -0,0 +1,19 @@
+def fourNumberSum(array, targetSum):
+	quadruplets=[]
+	allPairSums={}
+	for i in range(0,len(array)-1):
+		for j in range(i+1,len(array)):
+			currentSum=array[i]+array[j]
+			difference=targetSum-currentSum
+			if difference in allPairSums:
+				for pair in allPairSums[difference]:
+					quadruplets.append(pair+[array[i],array[j]])
+		for k in range(0,i):
+			currentSum=array[i]+array[k]
+			if currentSum not in allPairSums:
+				allPairSums[currentSum]=[[array[k], array[i]]]
+			else:
+				allPairSums[currentSum].append([array[k],array[i]])
+	return quadruplets
+				
+				
