21a19aece08785ab40c16760c9af8c9066c5aa39
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-09-08 19:15:49 +0530

    Update solution.py

--- Leet code/Same Tree/solution.py
+++ Leet code/Same Tree/solution.py
@@ -1,43 +1,16 @@
-def sameTree(arrayOne, arrayTwo):
-	t1=tree()
-	t2=tree()
-	
-	for i in arrayOne:
-		t1.insert(t1.root,i)
-	for i in arrayTwo:
-		t2.insert(t2.root,i)
-		
-	return t1.checkSame(t1.root,t2.root)
-	
-class tree:
-	def __init__(self):
-		self.root=None
-	
-	def insert(self,n,number):
-		if self.root==None:
-			self.root=node(number)
-		else:
-			if number<n.value:
-				if n.left==None:
-					n.left=node(number)
-				else:
-					self.insert(n.left,number)
-			else:
-				if n.right==None:
-					n.right=node(number)
-				else:
-					self.insert(n.right,number)
-					
-	def checkSame(self,n1,n2):
-		if n1==None and n2==None:
-			return True
-		elif n1!=n2:
-			return False
-		return self.checkSame(n1.left,n2.left) and self.checkSame(n1.right,n2.right)
-		
-class node:
-	def __init__(self, number):
-		self.value=number
-		self.right=None
-		self.left=None
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p==None and q==None:
+            return True
+        elif p==None or q==None:
+            return False
+        elif p.val!=q.val:
+            return False
+        return self.isSameTree(p.right,q.right) and self.isSameTree(p.left,q.left)
+        
