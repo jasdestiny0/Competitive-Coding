010c458d2bc3bd0dc3ebd47e3f44813a672f38b8
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-10-08 00:26:30 +0530

    commit

--- Code_Asylums/dsa_bootcamp/array_questions/triplet_sum.py
+++ Code_Asylums/dsa_bootcamp/array_questions/triplet_sum.py
@@ -1,8 +1,24 @@
 def triple_sum(arr, num):
-    closestDiff=float("inf")
-    soln=None
-    pairs={}
-    for i in range(0, len(num)-1, 1):
-        for j in range(i+1, len(num, 1)):
-            if num-arr[j] in pairs and abs(num-arr[j]:
-            pairs.add(arr[i]+arr[j])
+    if len(arr)<3:
+        return None
+    arr.sort()
+    smallestDiff=float("inf")
+    sumSoln=float("inf")
+    for i in range(0,len(arr)-1,1):
+        p1=i+1
+        p2=len(arr)-1
+        while p1<p2:
+            currentSum=arr[i]+arr[p1]+arr[p2]
+            if abs(num-currentSum)<smallestDiff:
+                smallestDiff=num-currentSum
+                sumSoln=currentSum
+            
+            if num<currentSum:
+                p1+=1
+            elif num>currentSum:
+                p2-=1
+            else:
+                break
+    return sumSoln
+
+print(triple_sum([-1, 2, 1, -4], 1))
