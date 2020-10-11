c8481aae57fa8e2cf1f80d3e2e911746ee46ffe7
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-10-11 17:00:05 +0530

    commit

---
+++ Code_Asylums/dsa_bootcamp/number_theory/ndiv.py
@@ -0,0 +1,11 @@
+import math as m
+def retModifiedSoe(n):
+    soe=[0 for i in range(0,n+1)]
+    for i in range(1,int((n+1)/2)+1,1):
+        for j in range(i, n+1, i):
+            soe[j]+=1
+    return soe
+
+def ndiv(n):
+    sieve=retModifiedSoe(n)
+    
