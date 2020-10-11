42c2d6f96bf1f00300be00d9701bc3899c611614
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-10-11 17:12:06 +0530

    commit

--- Code_Asylums/dsa_bootcamp/number_theory/ndiv.py
+++ Code_Asylums/dsa_bootcamp/number_theory/ndiv.py
@@ -1,7 +1,7 @@
 import math as m
 def retModifiedSoe(n):
     soe=[0 for i in range(0,n+1)]
-    for i in range(1,int((n+1)/2)+1,1):
+    for i in range(1,n+1,1):
         for j in range(i, n+1, i):
             soe[j]+=1
     return soe      
@@ -12,7 +12,6 @@ def ndiv(a,b,n):
     for i in range(a,b+1,1):
         if sieve[i]==n:
             count+=1
-    return count, sieve[1:]
-
+    return count
 print(ndiv(1,7,2))
 
