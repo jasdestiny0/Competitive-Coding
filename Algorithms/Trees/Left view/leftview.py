faa3eb00ecfcefe9c6d87a6dbf866592792b549f
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-10-13 08:43:42 +0530

    commit

---
+++ Algorithms/Trees/Left view/leftview.py
@@ -0,0 +1,53 @@
+class tree:
+    def __init__(self,val):
+        self.value=val
+        self.left=None
+        self.right=None
+
+    def inorder(self):
+        if self.left!=None:
+            self.left.inorder()
+        print(self.value)
+        if self.right!=None:
+            self.right.inorder()
+
+    def insert(self, val):
+        if self.left==None and self.value>val:
+            self.left=tree(val)
+        elif self.value>val:
+            self.left.insert(val)
+        elif self.right==None and self.value<=val:
+            self.right=tree(val)
+        else:
+            self.right.insert(val)
+
+    def leftview(self, currentDepth=1, exploredDepth=0):
+        if exploredDepth<currentDepth:
+            exploredDepth+=1
+            print(self.value)
+
+        if self.left!=None:
+            exploredDepth=self.left.leftview(currentDepth+1,exploredDepth)
+
+        if self.right!=None:
+            exploredDepth=self.right.leftview(currentDepth+1, exploredDepth)
+
+        return exploredDepth
+        
+
+t=tree(8)
+while True:
+    print("Enter option")
+    option=int(input())
+    if option==1:
+        print("Enter number")
+        n=int(input())
+        t.insert(n)
+        print("Printing the tree")
+        t.inorder()
+    if option==2:
+        print("Printing the left side only")
+        t.leftview()
+    if option==3:
+        break
+
