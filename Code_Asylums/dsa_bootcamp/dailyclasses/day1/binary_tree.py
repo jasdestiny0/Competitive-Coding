ff7e58b7a864794c3e40d9cf671ca854cdfe65f8
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-10-13 08:21:48 +0530

    commit

--- Code_Asylums/dsa_bootcamp/dailyclasses/day1/binary_tree.py
+++ Code_Asylums/dsa_bootcamp/dailyclasses/day1/binary_tree.py
@@ -18,16 +18,38 @@ class tree:
             self.left.insert(val)
         elif self.right==None and self.value<=val:
             self.right=tree(val)
-        elif self.right<=val:
+        else:
             self.right.insert(val)
-t=tree(1)
+
+    def leftview(self, currentDepth=1, exploredDepth=0):
+        exploredDepth1=exploredDepth
+        exploredDepth2=exploredDepth
+
+        if self.left!=None:
+            exploredDepth1=self.leftview(currentDepth+1,exploredDepth)
+    
+        if exploredDepth<currentDepth:
+            exploredDepth+=1
+        print(self.value)
+
+        if self.right!=None:
+            exploredDepth2=self.leftview(currentDepth+1, exploredDepth)
+
+        return max(exploredDepth, exploredDepth1, exploredDepth2)
+        
+
+t=tree(8)
 while True:
+    print("Enter option")
     option=int(input())
     if option==1:
+        print("Enter number")
         n=int(input())
         t.insert(n)
         print("Printing the tree")
         t.inorder()
     if option==2:
+        t.leftview()
+    if option==3:
         break
 
