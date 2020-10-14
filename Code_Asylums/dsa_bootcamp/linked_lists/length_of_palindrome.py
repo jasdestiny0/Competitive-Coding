1861e5b33d506d0b841fcc8d2e1fdf2826df786a
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-10-14 20:06:04 +0530

    commit

---
+++ Code_Asylums/dsa_bootcamp/linked_lists/length_of_palindrome.py
@@ -0,0 +1,42 @@
+class linked_list:
+    def __init__(self, node=None):
+        self.root=node
+    
+    def insert(self, val):
+        if self.root==None:
+            self.root=node(val)
+            return
+        n=self.root
+        while n.next!=None:
+            n=n.next
+        n.next=node(val)
+
+    def printLl(self):
+        print("Printing the linked list")
+        n=self.root
+        while n!=None:
+            print(n.value)
+            n=n.next
+
+    #calculate chain length
+    def turtle_and_hare(self):
+        turtle=self.root
+        hare=self.root.next
+        count=1
+        while turtle!=hare:
+            count+=1
+            turtle=turtle.next
+            hare=hare.next.next
+        return count    
+
+class node:
+    def __init__(self, val):
+        self.value=val
+        self.next=None
+
+
+
+ll=linked_list()
+
+
+while True:
