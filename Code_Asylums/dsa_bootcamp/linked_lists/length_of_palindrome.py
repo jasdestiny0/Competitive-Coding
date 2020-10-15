81f48d3719a40b7bda14694d2d7441f5ac19de8b
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-10-15 23:36:02 +0530

    c

--- Code_Asylums/dsa_bootcamp/linked_lists/length_of_palindrome.py
+++ Code_Asylums/dsa_bootcamp/linked_lists/length_of_palindrome.py
@@ -36,14 +36,39 @@ class linked_list:
                 n.next=n.next.next
                 continue
             n=n.next
+
+    def calcMaxLen(self, a, b):
+        len=0
+        while a!=None and b!=None:
+            if a.value==b.value:
+                len+=1
+            else:
+                break
+            a=a.next
+            b=b.next
+        return len
     
     def length_of_palindrome(self):
         if self.root.next==None:
             return 1
         prev=None
         n=self.root
+        maxlen=currlen1=currlen2=1
         while n!=None:
-            
+            temp=n.next
+            n.next=prev
+            prev=n
+            n=temp
+            if n!=None:
+                currlen1=2*self.calcMaxLen(temp, n)
+            else:
+                break
+
+            if n.next!=None:
+                currlen2=(2*self.calcMaxLen(temp, n.next))+1
+            maxlen=max(maxlen, currlen1, currlen2)
+        self.root=prev
+        return maxlen
 
 
 
@@ -58,11 +83,14 @@ while True:
     option=int(input("Enter option: "))
     if option==1:   
         number=int(input("Enter the number: "))
-        ll.insert(number)
+        ll.insert(number) 
         ll.printLl()
     #forming circular linked list
-    elif option==2:
+    elif option==2: 
         ll.remove_duplicates()
         ll.printLl()
+
+    elif option==3:
+        print(ll.length_of_palindrome())
     else:
         break
